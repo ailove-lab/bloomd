e5afabe8949747b8e8e1561edc7352daea9e22dd
peko <gasu.basu@gmail.com>
2016-02-29 18:44:35 +0300

    update

---
+++ utils/info-bloom.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <unistd.h>
+
+#include "bloom.h"
+
+  ////////////
+ /// MAIN ///
+////////////
+
+int main(int argc, char **argv) {
+
+    DIR *dp;
+    struct dirent *ep;
+    char filtername[256];
+
+    dp = opendir ("./blooms/");
+    if (dp != NULL) {
+        while((ep=readdir(dp)) != NULL) if(ep->d_name[0] != '.') {
+            sprintf(filtername,"./blooms/%s", ep->d_name);
+            bloom_load_info(filtername);
+        }
+        (void) closedir (dp);
+    } else fprintf(stderr, "Couldn't open the directory!");
+
+}
